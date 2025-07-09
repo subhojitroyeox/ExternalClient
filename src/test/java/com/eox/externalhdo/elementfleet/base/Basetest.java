73afadd6aa2720e003239a1a67cf02a47e129a90
tejar24 <tejaramesh@eoxvantage.com>
2025-07-09 15:44:43 +0530

    Initial commit

--- src/test/java/com/eox/externalhdo/elementfleet/base/Basetest.java
+++ src/test/java/com/eox/externalhdo/elementfleet/base/Basetest.java
@@ -1,5 +1,12 @@
 package com.eox.externalhdo.elementfleet.base;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
@@ -7,37 +14,24 @@ import org.testng.annotations.BeforeSuite;
 
 import com.eox.externalhdo.elementfleet.hdolutils.HDOLUtils;
 import com.eox.externalhdo.elementfleet.hdolutils.ElementUtils;
-import com.eox.externalhdo.elementfleet.pages.ApplicationAcknowledgementPage;
-import com.eox.externalhdo.elementfleet.pages.EsignPage;
-import com.eox.externalhdo.elementfleet.pages.TilevalidationPage;
-import com.eox.externalhdo.elementfleet.pages.UserManagementLaunchPage;
-import com.eox.externalhdo.elementfleet.test.ApplicationAcknowledgementTest;
 import com.eox.utils.CommonFunctionUtils;
 import com.eox.utils.HDOUtils;
 import com.eox.utils.SupportUtils;
 import com.eox.utils.WebDriverUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Basetest {
 	public static WebDriver driver;
 	public static CommonFunctionUtils cu;
-	public static ApplicationAcknowledgementPage AAT;
-	public static ElementUtils elementutils;
-	public static EsignPage esign;
-	public static UserManagementLaunchPage userpage;
-	public static HDOUtils hdoUtils;
-	public static ApplicationAcknowledgementPage apppage;
-	public static HDOLUtils pdfesign;
-	public static TilevalidationPage tile;
+	
 
 	@BeforeSuite
 	public static void initializebrowser() {
 		driver = WebDriverUtils.getDriver();
 		driver.get("https://hdoustest.eoxvantage.com/#login");
 		cu = new CommonFunctionUtils(driver, 10);
-		apppage = new ApplicationAcknowledgementPage(driver);
-		userpage = new UserManagementLaunchPage(driver);
-		pdfesign = new HDOLUtils(driver);
-		tile = new TilevalidationPage(driver);
+		
 
 	}
 
@@ -51,5 +45,15 @@ public class Basetest {
 //	{
 //		WebDriverUtils.quitDriver();
 //	}
+	public List<HashMap<String, String>> getJsonDataToMap(String filepath) throws IOException {
+		// read json to string
+		String jsonContent = FileUtils.readFileToString(new File(filepath), StandardCharsets.UTF_8);
+		// String to Json using Jackson databind
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String, String>> data = mapper.readValue(jsonContent,
+				new TypeReference<List<HashMap<String, String>>>() {
+				});
+		return data;
+	}
 
 }
